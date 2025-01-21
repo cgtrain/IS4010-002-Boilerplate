02c981bc75b293ad19e77fede790fa64d154a5c2
jldriehaus <driehajl@mail.uc.edu>
2025-01-21 08:32:13 -0500

    Made a random number generator

---
+++ IS4010-002 Boilerplate/mainPackage/driehajl.py
@@ -0,0 +1,7 @@
+
+import random
+
+random_number = random.randint(1, 100)
+
+print(f"The random number is: {random_number}")
+
--- IS4010-002 Boilerplate/mainPackage/main.py
+++ IS4010-002 Boilerplate/mainPackage/main.py
@@ -1,2 +1,8 @@
 # main.py
 
+if __name__ == "__main__": # Entry point check
+
+
+    from driehajl import *
+
+
